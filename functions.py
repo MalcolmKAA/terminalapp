579cc414e83e825e2a0909549c064737f6287d5e
MalcolmKAA <malcolmkaa@gmail.com>
2023-07-09 22:22:36 +1000

    expanded create workout function

--- functions.py
+++ functions.py
@@ -12,7 +12,14 @@ def create_exercise():
     return Exercise(name, sets, reps, weight, rest_time, weight_increase)
 
 def create_workout():
-
+ name = input("Enter workout name: ")
+    exercises = []
+    while True:
+        exercises.append(create_exercise())
+        add_more = input("Do you want to add more exercises to this workout? (yes/no): ")
+        if add_more.lower() != "yes":
+            break
+    return Workout(name, exercises)
 
 def show_workout_stats(workouts):
 
