f85db44708e9fd2d81f8581c83d4e4f74c8ec783
MalcolmKAA <malcolmkaa@gmail.com>
2023-07-09 17:10:23 +1000

    create main menu items

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ from functions import create_exercise, create_workout, show_workout_stats, save_
 
 def main():
     workouts = load_workouts()
+    while True:
+        print("1. Create a new workout")
+        print("2. Execute an existing workout")
+        print("3. Stats")
+        print("4. Export stats to CSV")
+        print("5. Save and exit")
+        user_input = input("Choose an option: ")
