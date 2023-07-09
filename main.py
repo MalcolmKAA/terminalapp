f761214a0c02decc21df6462398e3e581715594a
MalcolmKAA <malcolmkaa@gmail.com>
2023-07-09 20:09:22 +1000

    conditional statements for menu item 3-5

--- main.py
+++ main.py
@@ -21,3 +21,19 @@ def main():
             workout_number = int(input("Choose a workout to execute: ")) - 1
             if workout_number >= 0 and workout_number < len(workouts):
                 workouts[workout_number].execute_workout()
+            elif user_input == "3":
+            show_workout_stats(workouts)
+        elif user_input == "4":
+            filename = input("Enter filename (or leave empty for 'workout_stats.csv'): ")
+            if filename == "":
+                filename = "workout_stats.csv"
+            export_stats_to_csv(workouts, filename)
+        elif user_input == "5":
+            print("Saving and exiting...")
+            save_workouts(workouts)
+            break
+        else:
+            print("Invalid option. Please try again.")
+
+if __name__ == "__main__":
+    main()
