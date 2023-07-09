2215399e16234d29319ecd0b0436a5ff20f87849
MalcolmKAA <malcolmkaa@gmail.com>
2023-07-09 20:06:06 +1000

    conditional statments for menu item 1-2

--- main.py
+++ main.py
@@ -10,3 +10,14 @@ def main():
         print("4. Export stats to CSV")
         print("5. Save and exit")
         user_input = input("Choose an option: ")
+        if user_input == "1":
+            workouts.append(create_workout())
+        elif user_input == "2":
+            if not workouts:
+                print("No workouts found. Please create a workout first.")
+                continue
+            for i, workout in enumerate(workouts):
+                print(f"{i + 1}. {workout.name}")
+            workout_number = int(input("Choose a workout to execute: ")) - 1
+            if workout_number >= 0 and workout_number < len(workouts):
+                workouts[workout_number].execute_workout()
