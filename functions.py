3f92b8bcfe666469c4000014251cf444ac485e58
MalcolmKAA <malcolmkaa@gmail.com>
2023-07-09 22:15:30 +1000

    expanded create exercise function

--- functions.py
+++ functions.py
@@ -3,7 +3,13 @@ import csv
 from classes import Exercise, Workout
 
 def create_exercise():
-
+  name = input("Enter exercise name: ")
+    sets = int(input("Enter number of sets: "))
+    reps = int(input("Enter number of reps: "))
+    weight = float(input("Enter weight: "))
+    rest_time = int(input("Enter rest time between sets in seconds: "))
+    weight_increase = float(input("Enter weight increase after successful completion: "))
+    return Exercise(name, sets, reps, weight, rest_time, weight_increase)
 
 def create_workout():
 
