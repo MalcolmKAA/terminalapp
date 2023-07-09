df10813afbd483d5d1ceebff5b40b74c72707b33
MalcolmKAA <malcolmkaa@gmail.com>
2023-07-10 08:01:33 +1000

    added ability to exit during workout

--- classes.py
+++ classes.py
@@ -12,16 +12,16 @@ class Exercise:
         self.max_weight = weight
         self.date_max_weight = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        def increase_weight(self):
-            self.weight += self.weight_increase
-            if self.weight > self.max_weight:
-                self.max_weight = self.weight
-                self.date_max_weight = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    def increase_weight(self):
+        self.weight += self.weight_increase
+        if self.weight > self.max_weight:
+            self.max_weight = self.weight
+            self.date_max_weight = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        def decrease_weight(self, decrease_amount):
-            self.weight -= decrease_amount
-            if self.weight < 0:
-                self.weight = 0
+    def decrease_weight(self, decrease_amount):
+        self.weight -= decrease_amount
+        if self.weight < 0:
+            self.weight = 0
 
 class Workout:
     def __init__(self, name, exercises):
@@ -35,29 +35,42 @@ class Workout:
         for exercise in self.exercises:
             print(f"Starting {exercise.name}...")
             all_sets_successful = True
-            for set_number in range(1, exercise.sets + 1):
-                print(f"Set {set_number} of {exercise.sets}. {exercise.reps} reps at {exercise.weight}KG. Type 'exit' at anytime to leave the workout.")
-                user_input = input("Press 1 if completed, press 2 if failed: ")
+        for set_number in range(1, exercise.sets + 1):
+            print(f"Set {set_number} of {exercise.sets}. {exercise.reps} reps at {exercise.weight}KG. Type 'exit' at anytime to leave the workout.")
+            while True:  # loop until a valid input is entered
+                user_input = input("Press 1 if completed, press 2 if failed: ").lower()  # convert input to lowercase
+                if user_input not in {"1", "2", "exit"}:  # if input is not one of these options
+                    print("Invalid input. Please try again.")
+                else:  # if a valid input is entered, break the loop
+                    break
+
+            if user_input == "1":
+                print(f"Congratulations! Rest for {exercise.rest_time} seconds before next set.")
+                sleep(exercise.rest_time)
+            elif user_input == "2":
+                all_sets_successful = False
+                print("You failed the set. Would you like to decrease weight?")
+                while True:  # loop until a valid input is entered
+                    user_input = input("Press 1 for yes, 2 for no: ").lower()  # convert input to lowercase
+                    if user_input not in {"1", "2"}:  # if input is not one of these options
+                        print("Invalid input. Please try again.")
+                    else:  # if a valid input is entered, break the loop
+                        break
                 if user_input == "1":
-                    print(f"Congratulations! Rest for {exercise.rest_time} seconds before next set.")
-                    sleep(exercise.rest_time)
-                elif user_input == "2":
-                    all_sets_successful = False
-                    print(f"You failed the set. Would you like to decrease weight? ")
-                    user_input = input("Press 1 for yes, 2 for no: ")
-                    if user_input == "1":
-                        decrease_amount = int(input("Enter decrease amount: "))
-                        exercise.decrease_weight(decrease_amount)
-                        print(f"Weight has been decreased by {decrease_amount}. New weight is {exercise.weight}.")
-                elif user_input.lower() == "exit":
-                    print("Exiting the workout...")
-                    return
-            if all_sets_successful:
-                print(f"You have successfully completed {exercise.name}! Weight will be increased by {exercise.weight_increase} for next workout.")
-                exercise.increase_weight()
-        
-        def show_stats(self):
-            for exercise in self.exercises:
-                print(f"Exercise: {exercise.name}")
-                print(f"Max Weight Lifted: {exercise.max_weight}KG")
-                print(f"Date Max Weight Lifted: {exercise.date_max_weight}\n")
+                    decrease_amount = int(input("Enter decrease amount: "))
+                    exercise.decrease_weight(decrease_amount)
+                    print(f"Weight has been decreased by {decrease_amount}. New weight is {exercise.weight}.")
+            elif user_input == "exit":
+                print("Exiting the workout...")
+                return
+
+        if all_sets_successful:
+            print(f"You have successfully completed {exercise.name}! Weight will be increased by {exercise.weight_increase} for next workout.")
+            exercise.increase_weight()
+
+
+    def show_stats(self):
+        for exercise in self.exercises:
+            print(f"Exercise: {exercise.name}")
+            print(f"Max Weight Lifted: {exercise.max_weight}KG")
+            print(f"Date Max Weight Lifted: {exercise.date_max_weight}\n")
