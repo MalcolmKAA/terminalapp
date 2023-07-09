408d674cb47a15db342e70fe61a2c01f4373c4e9
MalcolmKAA <malcolmkaa@gmail.com>
2023-07-09 21:19:50 +1000

    added execute workout function to workout class

--- classes.py
+++ classes.py
@@ -30,3 +30,28 @@ class Workout:
 
     def add_exercise(self, exercise):
         self.exercises.append(exercise)
+
+    def execute_workout(self):
+        for exercise in self.exercises:
+            print(f"Starting {exercise.name}...")
+            all_sets_successful = True
+            for set_number in range(1, exercise.sets + 1):
+                print(f"Set {set_number} of {exercise.sets}. {exercise.reps} reps at {exercise.weight}KG. Type 'exit' at anytime to leave the workout.")
+                user_input = input("Press 1 if completed, press 2 if failed: ")
+                if user_input == "1":
+                    print(f"Congratulations! Rest for {exercise.rest_time} seconds before next set.")
+                    sleep(exercise.rest_time)
+                elif user_input == "2":
+                    all_sets_successful = False
+                    print(f"You failed the set. Would you like to decrease weight? ")
+                    user_input = input("Press 1 for yes, 2 for no: ")
+                    if user_input == "1":
+                        decrease_amount = int(input("Enter decrease amount: "))
+                        exercise.decrease_weight(decrease_amount)
+                        print(f"Weight has been decreased by {decrease_amount}. New weight is {exercise.weight}.")
+                elif user_input.lower() == "exit":
+                    print("Exiting the workout...")
+                    return
+            if all_sets_successful:
+                print(f"You have successfully completed {exercise.name}! Weight will be increased by {exercise.weight_increase} for next workout.")
+                exercise.increase_weight()
