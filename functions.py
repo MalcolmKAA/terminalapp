829fed314078737b7587b71369ee653fe07e1eae
MalcolmKAA <malcolmkaa@gmail.com>
2023-07-09 15:17:04 +1000

    added functions to be created

---
+++ functions.py
@@ -0,0 +1,19 @@
+
+
+def create_exercise():
+
+
+def create_workout():
+
+
+def show_workout_stats(workouts):
+
+
+def save_workouts(workouts):
+
+
+def load_workouts():
+
+
+def export_stats_to_csv(workouts, filename="workout_stats.csv"):
+
